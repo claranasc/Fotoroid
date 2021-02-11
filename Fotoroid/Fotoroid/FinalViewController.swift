c9bcf5f5b572fbd554c27e10b3cef8ab277c5bac
Clara Nascimento <clara.nascimento@outlook.com>
2021-02-11 19:31:21 -0300

    128.5 - Create class to EfeitosScene and ThirdScene.

---
+++ Fotoroid/Fotoroid/FinalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinalViewController.swift
+//  Fotoroid
+//
+//  Created by Clara Nascimento on 11/02/21.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
