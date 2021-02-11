1e8461426aa1ddab28e8589a266f6e38bb8ff0ec
Clara Nascimento <clara.nascimento@outlook.com>
2021-02-11 20:34:31 -0300

    129.1 - Add override func viewWillAppear in FinalViewController and ViewController; Add IBAction func selectSource and func selectPicture.

--- Fotoroid/Fotoroid/FinalViewController.swift
+++ Fotoroid/Fotoroid/FinalViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 class FinalViewController: UIViewController {
 
     @IBOutlet weak var ivPhoto: UIImageView!
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
     
     @IBAction func save(_ sender: UIButton) {
     }
--- Fotoroid/Fotoroid/ViewController.swift
+++ Fotoroid/Fotoroid/ViewController.swift
@@ -11,10 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
     @IBAction func selectSource(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar foto", message: "De onde você quer escolher a sua foto?", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default, handler: { (action) in
+                self.selectPicture(sourceType: .camera)
+            })
+            alert.addAction(cameraAction)
+        }
+        
+            let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action) in
+                self.selectPicture(sourceType: .photoLibrary)
+            }
+            alert.addAction(libraryAction)
+        
+            let photosAction = UIAlertAction(title: "Álbum de fotos", style: .default) { (action) in
+                self.selectPicture(sourceType: .savedPhotosAlbum)
+            }
+            alert.addAction(photosAction)
+        
+            let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+            alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func selectPicture(sourceType: UIImagePickerController.SourceType) {
+        
     }
     
 }
