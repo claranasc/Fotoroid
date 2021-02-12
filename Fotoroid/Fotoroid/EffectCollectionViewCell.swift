2ee6bfc4cbc461781b041acfe495dd99bab3fc5e
Clara Nascimento <clara.nascimento@outlook.com>
2021-02-12 19:43:31 -0300

    130.2 - Add extension EffectsViewController: UICollectionViewDataSource, UICollectionViewDelegate, add func numberOfSections, add func collectionView(numberOfItemsInSection section), add func collectionView(cellForItemAt indexPath in EffectsViewController; Add file EffectCollectionViewCell.swift.

---
+++ Fotoroid/Fotoroid/EffectCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EffectCollectionViewCell.swift
+//  Fotoroid
+//
+//  Created by Clara Nascimento on 12/02/21.
+//
+
+import UIKit
+
+class EffectCollectionViewCell: UICollectionViewCell {
+    
+}
